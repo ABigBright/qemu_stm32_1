58fba8ff74bec14ae626c1092b5e57b67109205d
lcgamboa <lcgamboa@yahoo.com>
2022-04-15 14:24:33 -0300

    use new glib context instead default and remove tcp sockets from picsimlab boards

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -132,6 +132,8 @@
 
 #define MAX_VIRTIO_CONSOLES 1
 
+extern GMainContext *g_main_context_default_l;
+
 typedef struct BlockdevOptionsQueueEntry {
     BlockdevOptions *bdo;
     Location loc;
@@ -1238,7 +1240,7 @@ static int chardev_init_func(void *opaque, QemuOpts *opts, Error **errp)
 {
     Error *local_err = NULL;
 
-    if (!qemu_chr_new_from_opts(opts, NULL, &local_err)) {
+    if (!qemu_chr_new_from_opts(opts, g_main_context_default_l, &local_err)) {
         if (local_err) {
             error_propagate(errp, local_err);
             return -1;
@@ -1456,7 +1458,7 @@ static int serial_parse(const char *devname)
     snprintf(label, sizeof(label), "serial%d", index);
     serial_hds = g_renew(Chardev *, serial_hds, index + 1);
 
-    serial_hds[index] = qemu_chr_new_mux_mon(label, devname, NULL);
+    serial_hds[index] = qemu_chr_new_mux_mon(label, devname, g_main_context_default_l);
     if (!serial_hds[index]) {
         error_report("could not connect serial device"
                      " to character backend '%s'", devname);
@@ -1487,7 +1489,7 @@ static int parallel_parse(const char *devname)
         exit(1);
     }
     snprintf(label, sizeof(label), "parallel%d", index);
-    parallel_hds[index] = qemu_chr_new_mux_mon(label, devname, NULL);
+    parallel_hds[index] = qemu_chr_new_mux_mon(label, devname, g_main_context_default_l);
     if (!parallel_hds[index]) {
         error_report("could not connect parallel device"
                      " to character backend '%s'", devname);
@@ -1501,7 +1503,7 @@ static int debugcon_parse(const char *devname)
 {
     QemuOpts *opts;
 
-    if (!qemu_chr_new_mux_mon("debugcon", devname, NULL)) {
+    if (!qemu_chr_new_mux_mon("debugcon", devname, g_main_context_default_l)) {
         error_report("invalid character backend '%s'", devname);
         exit(1);
     }
